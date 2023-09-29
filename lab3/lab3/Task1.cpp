3adf4f3e8e27c9df2426c570d2c8a9706554a3e0
krasilnikov30 <bkrasilnikov3006@gmail.com>
2023-09-29 06:56:11 +0300

    Исправления resharper

--- lab3/lab3/Task1.cpp
+++ lab3/lab3/Task1.cpp
@@ -9,11 +9,11 @@ constexpr unsigned WINDOW_HEIGHT = 600;
 
 int main()
 {
-    constexpr float BALL_SIZE = 40;
+	constexpr float BALL_SIZE = 40;
 
-    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Wave Moving Ball", sf::Style::Default);
-    sf::Clock iterClock;
-    sf::Clock analitycalClock;
+	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Wave Moving Ball", sf::Style::Default);
+	sf::Clock iterClock;
+	sf::Clock analitycalClock;
 
     window.setVerticalSyncEnabled(true);
 
@@ -23,35 +23,36 @@ int main()
     ball.setFillColor(sf::Color::Red);
 
     float speedX = 300;
-    constexpr float amplitudeY = 80;
-    constexpr float period = 2;
+	constexpr float period = 2;
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	    constexpr float amplitudeY = 80;
+	    sf::Event event;
+	    while (window.pollEvent(event))
+	    {
+		    if (event.type == sf::Event::Closed)
+		    {
+			    window.close();
+		    }
+	    }
 
-        if (position.x > WINDOW_WIDTH - 2 * BALL_SIZE || position.x < 0)
-        {
-            speedX *= -1;
-        }
+	    if (position.x > WINDOW_WIDTH - 2 * BALL_SIZE || position.x < 0)
+	    {
+		    speedX *= -1;
+	    }
 
-        const float iterTime = iterClock.restart().asSeconds();
-        const float analitycalTime = analitycalClock.getElapsedTime().asSeconds();
-        const float wavePhase = analitycalTime * float(2 * M_PI);
-        const float x = speedX * iterTime;
-        const float y = amplitudeY * std::sin(wavePhase / period);
-        position.x += x;
-        ball.setPosition(position + sf::Vector2f{ 0, y });
-        
+	    const float iterTime = iterClock.restart().asSeconds();
+	    const float analitycalTime = analitycalClock.getElapsedTime().asSeconds();
+	    const float wavePhase = analitycalTime * static_cast<float>(2 * M_PI);
+	    const float x = speedX * iterTime;
+	    const float y = amplitudeY * std::sin(wavePhase / period);
+	    position.x += x;
+	    ball.setPosition(position + sf::Vector2f{ 0, y });
 
-        window.clear(sf::Color::Black);
-        window.draw(ball);
-        window.display();
+	    window.clear(sf::Color::Black);
+	    window.draw(ball);
+	    window.display();
     }
     return 0;
 }
